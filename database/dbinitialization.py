03778b19444c2b0e404928c5b456a813dac9ee4a
Muhammad Usman <usmanshoaib537@gmail.com>
2023-10-14 08:21:26 +0500

    Update Model class of user

--- database/dbinitialization.py
+++ database/dbinitialization.py
@@ -3,6 +3,5 @@ from flask_mongoengine import MongoEngine
 # instantiate mongo engine
 db = MongoEngine()
 
-
 def initialize_db(app):
     db.init_app(app)
--- database/models.py
+++ database/models.py
@@ -4,6 +4,10 @@ from .dbinitialization import db
 class User(db.Document):
     name = db.StringField(required=True)
     email = db.EmailField(required=True)
-    gender = db.StringField()
-    current_semester = db.IntField()
+    gender = db.StringField(required=True)
+    rollno = db.StringField(required=True)
+    batch = db.StringField(required=True)
+    degree = db.StringField(required=True)
+    current_semester = db.IntField(required=True)
     tag = db.StringField(required=True)
+
