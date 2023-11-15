d3c8096560581705e2a1271e190e1a318e98046c
Muhammad Usman <usmanshoaib537@gmail.com>
2023-11-15 18:17:04 +0500

    Delete previous files

--- resources/routes.py
+++
@@ -1,8 +0,0 @@
-from .resources import *
-
-
-def initialize_routes(api):
-
-    api.add_resource(UserSignup, "/api/UserSignup/post_request")
-    api.add_resource(UserLogin, "/api/UserLogin/post_request")
-
