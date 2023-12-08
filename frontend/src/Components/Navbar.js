d27cac8bf8ee58694e9686777431a43ef181b910
mkb <muhammadkhizranbutt@gmail.com>
2023-12-08 21:47:39 +0500

    Navbar completed

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,47 +1,56 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbarBackground">
         <div className="container-fluid ">
-          <Link className="navbar-brand navbarTitle " to="#">PuConnect</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <NavLink className="navbar-brand navbarTitle activeLink" to="/">
+            PuConnect
+          </NavLink>
+          <button
+            className="navbar-toggler "
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link navbarText" aria-current="page" to="#">Home</Link>
+                <NavLink className="nav-link navbarText activeLink" to="/">
+                  Home
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link navbarText" to="#">Contact Us</Link>
+                <NavLink className="nav-link navbarText activeLink" to="/contact">
+                  Contact Us
+                </NavLink>
               </li>
-              {/* <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" to="#">Action</a></li>
-                  <li><a className="dropdown-item" to="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" to="#">Something else here</a></li>
-                </ul>
-              </li> */}
-               <li className="nav-item">
-                <Link className="nav-link navbarText" to="#">About Us</Link>
+              <li className="nav-item">
+                <NavLink className="nav-link navbarText activeLink" to="/about">
+                  About Us
+                </NavLink>
               </li>
             </ul>
-            <div className="d-flex" >
-                <button className=" skinButton me-2" type="submit">SignUp</button>              
-                <button className="skinButton me-2" type="submit">LogIn</button>
+            <div className="d-flex">
+              <button className="greyButton me-2" type="submit">
+                SignUp
+              </button>
+              <button className="greyButton me-2" type="submit">
+                LogIn
+              </button>
             </div>
           </div>
         </div>
       </nav>
-
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
