cf2a6bfc15fca9011386fbf63e78b67d5c927956
mkb <muhammadkhizranbutt@gmail.com>
2023-12-10 18:33:20 +0500

    Home Page done

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -4,11 +4,20 @@ const Home = () => {
   return (
     <>
       <section>
-      <div className='background'>
-           <div className='p-5 ms-5' >
-              <span className='welComeLine' >Welcome to PuConnect</span>
-           </div>
-
+        <div className='background'>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 ps-3 pt-5 ms-5">
+                <span className="welComeLine">Welcome to PuConnect</span>
+              </div>
+              <div className="col-md-6 ps-3 pt-4 ms-5">
+                <span className="tagLine">Your Networking Gateway</span>
+              </div>
+              <div className="col-md-6 ps-4 pt-4 ms-5">
+                <span className="getStartedButton">Get Sarted</span>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
