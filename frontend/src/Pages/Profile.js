eace0ede274da56b288125ece5e294fc78f4f595
mkb <muhammadkhizranbutt@gmail.com>
2024-01-04 00:20:17 +0500

    Working on Banner

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -26,16 +26,16 @@ const Profile = () => {
       console.error('No file selected');
       return;
     }
-  
+
     if (!userId) {
       console.error('User ID is undefined');
       return;
     }
-  
+
     try {
       const formData = new FormData();
       formData.append('image', selectedFile);
-  
+
       const response = await fetch(`http://localhost:3000/bg-banner/${userId}`, {
         method: 'POST',
         body: formData,
@@ -43,23 +43,23 @@ const Profile = () => {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
       });
-  
+
       if (response.ok) {
         // Assuming your API returns the updated user object
         const updatedUser = await response.json();
-  
+
         // Dispatch the login action with the updated user object
         dispatch(login(updatedUser));
-  
+
         // Update the local state with the new user object
         setUserId(updatedUser._id);
-  
+
         // Fetch the banner image URL separately
         const bannerImageResponse = await fetch(`http://localhost:3000/bg-banner/${userId}`);
         const bannerImage = await bannerImageResponse.json();
-  
+
         console.log('Banner uploaded Successfully', bannerImage.imageUrl);
-  
+
       } else {
         console.error('Error uploading banner', response.statusText);
       }
@@ -67,12 +67,13 @@ const Profile = () => {
       console.error('Error uploading banner', error);
     }
   };
-  
+
 
   useEffect(() => {
     // Log the banner image URL after the user state is updated
     if (user && user.bannerImageUrl) {
       console.log('Banner Image URL:', `http://localhost:3000/${user.bannerImageUrl}`);
+      
     }
   }, [user]);
 
@@ -93,6 +94,7 @@ const Profile = () => {
 
   return (
     <>
+      
       {/* Display the user's banner image */}
       {user && user.bannerImageUrl && (
         <img src={`http://localhost:3000/${user.bannerImageUrl}`} alt="User Banner" />
