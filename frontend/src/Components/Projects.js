21711ffa77090724da2e0795826ffa374cca3e43
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-01-31 14:54:13 +0500

    Working on project component

--- frontend/src/Components/Projects.js
+++ frontend/src/Components/Projects.js
@@ -1,17 +1,42 @@
 import React from "react";
+import { FaPen, FaPlus } from "react-icons/fa";
 
 const Projects = () => {
   return (
-    <div className="container ">
+    <div className="container">
       <div className="row">
         <div className="col col-9">
           <div className="card mb-3">
-            <div className="card-body">
-              <h5 className="">Projects</h5>
-              <p className="card-text">
-                Add projects
-              </p>
-              
+            <div className="card-body ">
+              <div className="d-flex justify-content-between align-items-center">
+                <h5 className="mb-0">Projects</h5>
+                <div className="d-flex">
+                  <FaPlus
+                    className="me-2"
+                    size={22}
+                    style={{
+                      cursor: "pointer",
+                      backgroundColor: "white",
+                      padding: "4px",
+                    }}
+                    // onClick={handlePlusClick}
+                  />
+                  <FaPen
+                    size={22}
+                    style={{
+                      cursor: "pointer",
+                      backgroundColor: "white",
+                      padding: "4px",
+                    }}
+                    // onClick={handlePenClick}
+                  />
+                </div>
+              </div>
+
+              <p className="card-text mt-4">Add projects</p>
+
+            </div>
+            <div>
             </div>
           </div>
         </div>
