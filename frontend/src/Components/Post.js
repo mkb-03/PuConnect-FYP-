70277594c8188ec89b2f8a35646e9c15a66cc392
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-03-06 22:21:59 +0500

    working on default post

--- frontend/src/Components/Post.js
+++ frontend/src/Components/Post.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Post = () => {
   return (
-    <div>Post</div>
-  )
-}
+    <div>
+      <div class="card mt-4 me-5">
+        <div class="card-body">
+        <h5 class="card-title">New Post</h5>
+          <p class="card-text">
+            <span>
+                
+            </span>
+            <input type="text" placeholder="What's in your mind?">
+            </input>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
