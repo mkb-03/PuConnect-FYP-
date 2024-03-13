8750178465d1b911478d229d42da1df2fabf7f22
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-03-14 03:01:25 +0500

    started working on post

--- frontend/src/Components/Posts.js
+++ frontend/src/Components/Posts.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
-}
+  const token = useSelector((state) => state.auth.token);
+  const [allPosts, setAllPosts] = useState([]);
+  const [userPosts, setUserPosts] = useState([]);
 
-export default Posts
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/post/getAll", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setAllPosts(response.data);
+        console.log(response.data)
+      } catch (error) {
+        console.log("Error fetching all posts", error);
+      }
+    };
+
+    const fetchUserPosts = async ()=>{
+        try {
+            const response = await axios.get("http://localhost:3000/post/getAll", {
+                headers : {
+                    Authorization : `Bearer ${token}`
+                }
+            })
+
+            setUserPosts(response.data)
+            console.log("User Posts: " , response.data);
+            
+        } catch (error) {
+            console.log("Error fetching user posts", error);
+        }
+    }
+
+    fetchAllPosts();
+  }, [token]);
+
+
+  return <div>
+    {allPosts.map((post)=>(
+        <div key={post._id}>
+            <div>{post.description}</div>
+        </div>
+    ))}
+  </div>;
+};
+
+export default Posts;
