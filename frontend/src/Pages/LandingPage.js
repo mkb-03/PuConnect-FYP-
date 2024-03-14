79be7396525a7fedc122556edf521471c1dd5a55
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-03-15 03:03:32 +0500

    working on landing page

--- frontend/src/Pages/LandingPage.js
+++ frontend/src/Pages/LandingPage.js
@@ -26,14 +26,36 @@ const LandingPage = () => {
 
       <section>
         <div className="container pt-5 pb-5 ">
-          <div className="headings brownColor">Why PuConnect?</div>
-          <div class="container  ">
-            <div class="row pt-5  ">
-              <div class="col">
-                <img src="" alt="whyPic"/>
+          <div className="headings brownColor ps-3">Why PuConnect?</div>
+          <div className="container  ">
+            <div className="row pt-5  ">
+              <div className="col text-center">
+                <img
+                className="whyPuC "
+                  src={process.env.PUBLIC_URL + "/images/whyPuConnect.jpg"}
+                  alt="whyPic"
+                />
               </div>
-              <div class="col">
-                
+              <div className="col">
+                <div>
+                  <h5 className="brownColor pt-4">Strong Comunity</h5>
+                  <p className="pt-1">
+                    Join an interactive group of talented and creative minds.
+                  </p>
+                </div>
+                <div>
+                  <h5 className="brownColor pt-3">Innovative Tools</h5>
+                  <p className="pt-1">
+                    Harness the power of our premium collaborative resources.
+                  </p>
+                </div>
+                <div>
+                  <h5 className="brownColor pt-3">Career Opportunities</h5>
+                  <p className="pt-1">
+                    Explore a wealth of work opportunities in leading
+                    industries.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
