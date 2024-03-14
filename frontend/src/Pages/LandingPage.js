9ae64f36633429dbe2fea85482f3ca23aa4da2eb
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-03-15 03:11:19 +0500

    working on landing page

--- frontend/src/Pages/LandingPage.js
+++ frontend/src/Pages/LandingPage.js
@@ -31,7 +31,7 @@ const LandingPage = () => {
             <div className="row pt-5  ">
               <div className="col text-center">
                 <img
-                className="whyPuC "
+                  className="whyPuC "
                   src={process.env.PUBLIC_URL + "/images/whyPuConnect.jpg"}
                   alt="whyPic"
                 />
@@ -39,23 +39,43 @@ const LandingPage = () => {
               <div className="col">
                 <div>
                   <h5 className="brownColor pt-4">Strong Comunity</h5>
-                  <p className="pt-1">
+                  <p className="pt-1" style={{fontWeight: "500"}}>
                     Join an interactive group of talented and creative minds.
                   </p>
                 </div>
                 <div>
                   <h5 className="brownColor pt-3">Innovative Tools</h5>
-                  <p className="pt-1">
+                  <p className="pt-1" style={{fontWeight: "500"}}>
                     Harness the power of our premium collaborative resources.
                   </p>
                 </div>
                 <div>
                   <h5 className="brownColor pt-3">Career Opportunities</h5>
-                  <p className="pt-1">
+                  <p className="pt-1" style={{fontWeight: "500"}}>
                     Explore a wealth of work opportunities in leading
                     industries.
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container pt-5 pb-5 ">
+          <div className="container  ">
+            <div className="row pt-5  ">
+              <div className="col ">
+                <div className="headings ps-3">Get to Know Us</div>
+              </div>
+              <div className="col pe-5">
+                <div>
+                  <p className="pt-1 " style={{fontWeight: "600"}}>
+                  We're not just a portal, we're the launchpad for your next big discovery. Connect with classmates, participate in stimulating discussions, find study groups, and even career opportunities. With PuConnect, it’s more than an education, it’s an experience.
+                  </p>
+                </div>
+                
               </div>
             </div>
           </div>
